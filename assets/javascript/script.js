482c4e19cfbf1860efade40ec8de7c3c76635893
Dana Cronin <cronin43224235@gmail.com>
2019-10-29 23:10:08 -0400

    googles youtube-data api-key

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
 var ingredients = [];
+const youtubeKey = 'AIzaSyCyE6uRr4N0thLeeGRFNJvNkVm4o4sSbBo';
 $('.chips').chips(); // making 'chips' appear (line 24 of html input / retrieve data to add to list
 
