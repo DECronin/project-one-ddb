3762458044843deec885cdd49bd49eb96d467125
Dana Cronin <cronin43224235@gmail.com>
2019-11-02 13:06:47 -0400

    linking glide.js and structure (not complete)

--- assets/javascript/video.js
+++ assets/javascript/video.js
@@ -1,6 +1,6 @@
 const youtubeKey = 'AIzaSyCyE6uRr4N0thLeeGRFNJvNkVm4o4sSbBo';
-
 let input;
+new Glide('.glide').mount();
 
 function getVideo() {
     console.log('vid-input = ' + input);
@@ -28,7 +28,7 @@ function getVideo() {
     console.log(data);
     let list = $('<div>');
     for (i = 0; i < 10; i++){
-        var newDiv = $('<div class="video-results">');
+        var newDiv = $('<li class="video-results" class="glide__slide">');
         var iframe = $('<iframe src="">');
         var h3 = $('<h3>');
         var desc = $('<p class="description">');
