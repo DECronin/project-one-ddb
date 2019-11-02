90dd51c684b7bc70a2c0f319f1d597adddeae7f6
Dana Cronin <cronin43224235@gmail.com>
2019-11-02 10:57:39 -0400

    validating fields filled before sending comment

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -1,12 +1,32 @@
+function validate(user, email, comment) {
+      
+    if( user == "" ) {
+       alert( "Please provide your name!" );
+       $('#name-box').css('background-color', 'red');
+    }
+    if( email == "" ) {
+       alert( "Please provide your Email!" );
+       $('#email-box').css('background-color', 'red');
+    }
+    if( comment == "" ) {
+       alert( "Please provide your comment!" );
+       $('#comment-box').css('background-color', 'red');
+    }
+ }
+
 $('#send-comment').on('click', function(){
     event.preventDefault();
     
     var user = $('#username-input').val();
     var email = $('#email-input').val();
     var comment = $('#comment-input').val();
-    console.log("=========");
-    console.log(user);
-    console.log(email);
-    console.log(comment);
+
+    if (user === '' || email === '' || comment === ''){
+        alert('Please fill in all fields before sending comment');
+        validate(user, email, comment);
+    } else {
+        console.log('sending comment: ' + user + '//' + email + '//' + comment);
+        // sendComment();
+    }
 });
 
