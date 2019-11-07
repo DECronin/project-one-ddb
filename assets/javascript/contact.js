ce6b25ce54821428fb987ac1fdb0e35aecd236c2
Dana Cronin <cronin43224235@gmail.com>
2019-11-06 19:08:27 -0500

    fixed bugg

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -1,23 +1,21 @@
-
-
 var user;
 var email;
 var comment;
-
 function validate() {
     $('#empty-contact').empty();
     if (user == "" ) {
         $('#empty-contact').append('* Please provide a valid Name.');
         $('#username-input').addClass('invalid');
-    } if (email == "" || !email.includes('@') || !email.includes('.')) {
+    } 
+    if (email == "" || !email.includes('@') || !email.includes('.')) {
         $('#empty-contact').append('<br>* Please provide a valid Email Address.');
         $('#email-input').addClass('invalid');
-    } if (comment == "" ) {
+    } 
+    if (comment == "" ) {
         $('#empty-contact').append('<br>* Please provide a Comment.');
         $('#comment-input').addClass('invalid');
     }
  }
-
  function sendComment(){
     database.ref().push({
         name: user,
@@ -28,7 +26,6 @@ function validate() {
     $('#email-input').val('');
     $('#comment-input').val('');
  }
-
 $('#send-comment').on('click', function(){
     event.preventDefault();
     $('#empty-contact').empty();
@@ -38,13 +35,12 @@ $('#send-comment').on('click', function(){
     email = $('#email-input').val();
     comment = $('#comment-input').val();
 
-    if (user === '' || email === '' || comment === ''){
+    if (user === '' || email === '' || comment === '' || !email.includes('@') || !email.includes('.')){
         validate();
     } else {
         sendComment();
     }
 });
-
 //database.ref().on('child_added', function (snapshot) {
     //console.log('snapshot' + JSON.stringify(snapshot));
     //send emails
